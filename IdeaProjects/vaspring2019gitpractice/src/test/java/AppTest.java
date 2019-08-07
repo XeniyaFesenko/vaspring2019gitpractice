b52549b0293ba6779f6fa437acb2d0a8f0a5c94c
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2019-08-06 20:04:01 -0400

    Update AppTest.java

--- IdeaProjects/vaspring2019gitpractice/src/test/java/AppTest.java
+++ IdeaProjects/vaspring2019gitpractice/src/test/java/AppTest.java
@@ -2,7 +2,7 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println("hello2");
+        
 
     }
 }
